152a4fe6a2b566b75de000a2696b1ad6aa0d5ea3
Abhinav-grd <abhinavk.grd@gmail.com>
2021-03-25 14:54:57 +0530

    remove worker loader and module rules

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -11,14 +11,6 @@ module.exports = {
                 })
             );
         }
-        config.module.rules.push({
-            test: /\.worker\.js$/,
-            loader: 'worker-loader',
-            options: {
-                filename: 'static/[hash].worker.js',
-                publicPath: '/_next/',
-            },
-        });
         return config;
     },
 };
