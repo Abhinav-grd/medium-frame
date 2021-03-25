04d39068a264bcbfd9da7a8f620729eee513455c
Abhinav-grd <abhinavk.grd@gmail.com>
2021-03-25 15:29:30 +0530

    removed isServer check while loading worker plugin

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -3,14 +3,12 @@ const WorkerPlugin = require('worker-plugin');
 module.exports = {
     webpack: (config, options) => {
         config.target = 'electron-renderer';
-        if (options.isServer) {
-            config.plugins.push(
-                new WorkerPlugin({
-                    // use "self" as the global object when receiving hot updates.
-                    globalObject: 'self',
-                })
-            );
-        }
+        config.plugins.push(
+            new WorkerPlugin({
+                // use "self" as the global object when receiving hot updates.
+                globalObject: 'self',
+            })
+        );
         return config;
     },
 };
